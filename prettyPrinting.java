e9cd2cca66184b33ad87ffa33fc3cf397979b0c1
Bhavya Soni <bhavyasoni9587@gmail.com>
2025-09-06 23:59:05 +0530

    pretty formatting

---
+++ prettyPrinting.java
@@ -0,0 +1,16 @@
+public class prettyPrinting {
+    public static void main(String[] args) {
+
+        float a = 212.123434f;
+        System.out.printf("Formatted Float is %.2f",a); //round of as well
+
+        System.out.printf("\nPie: %.3f", Math.PI);
+
+        System.out.printf("\nHello my name is %s and maybe i am %s dumb ", "Something", "So" );
+
+
+
+
+    }
+    
+}
